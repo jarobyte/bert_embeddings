dbe98926c22b78b52cac8363fc999bf350ba5e4d
Juan <jarobyte91@gmail.com>
2020-03-23 15:13:00 -0300

    diagnostics added, reorganized code, the issue of producing more embeddings was solved

--- produce_embeddings.py
+++ produce_embeddings.py
@@ -9,58 +9,22 @@ import argparse
 import re
 import shutil
 from timeit import default_timer
-
-
-start = default_timer()
-
-parser = argparse.ArgumentParser()
-parser.add_argument('path')
-parser.add_argument('batch_size', type = int)
-args = parser.parse_args()
-
-batch_size = args.batch_size
-
-df_path = args.path
-folder, file_name = re.match(r'(^.*)/(.*)\.', df_path).groups()
-output_folder = '{}/{}_bert'.format(folder, file_name)
-
-print('cleaning folders...')
-try:
-    shutil.rmtree(output_folder)
-except:
-    print('ok')
-
-
-print('creating folders...')
-try:
-    os.mkdir(output_folder)
-except:
-    print('main folder created succesfully...')
-
-try:
-    os.mkdir('{}/tmp'.format(output_folder))
-except:
-    print('tmp folder created succesfully...')
+import functools as ft
     
-try:
-    os.mkdir('{}/json'.format(output_folder))
-except:
-    print('json folder created succesfully...')
-    
-try:
-    os.mkdir('{}/embeddings'.format(output_folder))
-except:
-    print('embedding folder created succesfully...')
-    
-def extract_cls_embedding(text_list, 
-                          data_folder,
-                          tmp_file_name = 'tmp', 
-                          tmp_folder = 'tmp', 
-                          out_folder = 'json',
-                          bert_folder = 'bert_resources',
-                          bert_models_folder = 'bert_models',
-                          bert_model = 'uncased_L-2_H-128_A-2'):     
+def extract_cls_embeddings(text_list, 
+                           data_folder,
+                           tmp_file_name = 'tmp', 
+                           tmp_folder = 'tmp', 
+                           out_folder = 'json',
+                           bert_folder = 'bert_resources',
+                           bert_models_folder = 'bert_models',
+                           bert_model = 'uncased_L-2_H-128_A-2'):     
 
+    """
+    Warning: this function may produce more embeddings than elements in text_list 
+    because the text is written to disk and the read back by BERT
+    """
+    
     with open('{}/{}/{}.txt'.format(data_folder, tmp_folder, tmp_file_name), 'w+') as file:
         for l in text_list:
             file.write(l)
@@ -82,8 +46,8 @@ def extract_cls_embedding(text_list,
                                "--layers=-1",
                                "--max_seq_length=128",
                                "--batch_size=8"])
-    if process.returncode != 0:
-        raise ValueError('There was a problem with the subprocess')
+#     if process.returncode != 0:
+#         raise ValueError('There was a problem with the subprocess')
         
     with open('{}/{}/{}.jsonl'.format(data_folder, out_folder, tmp_file_name), 'r') as file:
         jsons = [json.loads(l) for l in file.readlines()]
@@ -101,17 +65,60 @@ def produce_batches(data, batch_size):
               for i, b in enumerate(batches)]
     return output
 
-
 def extract_emb(x):
-    return (x[0], extract_cls_embedding(x[1], tmp_file_name = x[0], data_folder = output_folder))
+    return (x[0], extract_cls_embeddings(x[1], tmp_file_name = x[0], data_folder = output_folder))
 
+
+
+start = default_timer()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('path')
+parser.add_argument('batch_size', type = int)
+args = parser.parse_args()
+
+batch_size = args.batch_size
+
+df_path = args.path
+folder, file_name = re.match(r'(^.*)/(.*)\.', df_path).groups()
+output_folder = '{}/{}_bert'.format(folder, file_name)
+
+print('removing folders...')
+try:
+    shutil.rmtree(output_folder)
+except FileNotFoundError:
+    print('removing not needed')
+
+
+print('creating folders...')
+try:
+    os.mkdir(output_folder)
+    os.mkdir('{}/tmp'.format(output_folder))
+    os.mkdir('{}/json'.format(output_folder))
+    os.mkdir('{}/embeddings'.format(output_folder))
+except:
+    print('there was an error while creating the folders')
+    
 df = pd.read_csv(df_path)
 batches = produce_batches(df['text'].tolist(), batch_size)
+
+# sequential version of the imap_unordered, useful for debugging
+# results = []
+# for n, b in batches:
+#     results.append((n, extract_cls_embeddings(b, tmp_file_name = n, data_folder = output_folder)))
+
 p = mp.Pool(processes = os.cpu_count() - 1)
 results = list(p.imap_unordered(extract_emb, tqdm.tqdm(batches)))
+
 arrays = [(b, np.stack(l)) for b, l in sorted(results)]
+total_embeddings = 0
 for b, a in arrays:
-    np.save('{}/embeddings/{}.npy'.format(output_folder, b), a)
-    
-print()
-print('Total processing time: {}'.format(default_timer() - start))
+    np.save('{}/embeddings/{}_{}x{}.npy'.format(output_folder, b, a.shape[0], a.shape[1]), 
+            a)
+    total_embeddings += a.shape[0]
+
+for i in range(5):    
+    print()
+print('Total processing time: {:.1f} sec'.format(default_timer() - start))
+print('Total records in  df["text"]: {}'.format(df['text'].size))
+print('Total embeddings produced: {}'.format(total_embeddings))
